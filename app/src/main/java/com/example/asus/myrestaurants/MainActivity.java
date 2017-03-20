6080d946c6ea3755e1477cb75bbe119268ba8991
Resident Mujahid <mujahidexpert@gmail.com>
2017-03-20 17:10:37 +0300

    after adding signpost

--- app/src/main/java/com/example/asus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/asus/myrestaurants/MainActivity.java
@@ -13,10 +13,13 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 
-public class MainActivity extends AppCompatActivity {
-    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
-    @Bind(R.id.locationEditText) EditText mLocationEditText;
-    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.findRestaurantsButton)
+    Button mFindRestaurantsButton;
+    @Bind(R.id.locationEditText)
+    EditText mLocationEditText;
+    @Bind(R.id.appNameTextView)
+    TextView mAppNameTextView;
 
 
     @Override
@@ -29,14 +32,17 @@ public class MainActivity extends AppCompatActivity {
 
         mAppNameTextView.setTypeface(brushFont);
 
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+        mFindRestaurantsButton.setOnClickListener(this);
+    }
+
     @Override
     public void onClick(View v) {
-        String location = mLocationEditText.getText().toString();
-        Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-        intent.putExtra("location", location);
-        startActivity(intent);
-    }
-        });
+        if (v == mFindRestaurantsButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
+
